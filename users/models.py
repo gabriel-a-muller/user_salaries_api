ac65bbf3b26fc1a22351cc23125b115d55b97c15
Gabriel Müller <gabrielgoetzmuller@gmail.com>
2021-10-02 17:28:17 -0300

    FEAT: Two new Apps. User App with tests, model and migrations

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db.models.fields import CharField, DateField
+from django.db.models.fields import (
+    CharField,
+    DateField
+)
+from cpf_field.models import CPFField
+
+
+class User(models.Model):
+    cpf = CPFField('cpf', unique=True)
+    name = CharField(max_length=155, verbose_name='Name')
+    born_date = DateField()
